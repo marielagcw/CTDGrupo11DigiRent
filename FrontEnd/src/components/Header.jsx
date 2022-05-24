77838774262f4ff74ca5335c59da43023308caf6
AguRivarola <agurivarola@gmail.com>
2022-05-24 02:30:52 -0300

    Terminado Responsive mobile sin funcinalidad de hamburguesa

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -10,29 +10,31 @@ const Header = () => {
 
   const handleCreateAccountLogIn = (e) => {
     displayForm(e)
-    
+
   }
 
 
   return (
-      <>
-        <nav className='navbar navbar-light'>
-          <div className="container-fluid">
-            <div className='d-flex align-items-center'>
-                <div className="image-container d-flex align-items-center">
-                  <img src={logo} className="img-fluid" alt="logo" />
-                </div>
-                <p className='ps-2 fw-bold mb-0'>Digi <span className='bg-tertiary fw-bold text-light ps-1 pe-1'>Rent</span></p>
+    <>
+      <nav className='navbar navbar-light'>
+        <div className="container-fluid">
+          <div className='d-flex align-items-center'>
+            <div className="image-container d-flex align-items-center">
+              <img src={logo} className="img-fluid" alt="logo" />
             </div>
-            <div>
-                <button className='btn btn-lg btn-border-primary' id='createAcount' onClick={handleCreateAccountLogIn}>Crear cuenta</button>
-                <button className='btn btn-lg btn-border-primary ms-2 me-2' id='logIn' onClick={handleCreateAccountLogIn}>Iniciar sesión</button>
-            </div>
+            <p className='ps-2 fw-bold mb-0'>Digi <span className='bg-tertiary fw-bold text-light ps-1 pe-1'>Rent</span></p>
           </div>
-        </nav>
-        <RegistreForm />
-        <LogInForm/>
-      </>
+          <div className='session-manager'>
+            <button className='btn btn-lg btn-border-primary' id='createAcount' onClick={handleCreateAccountLogIn}>Crear cuenta</button>
+            <button className='btn btn-lg btn-border-primary ms-2 me-2' id='logIn' onClick={handleCreateAccountLogIn}>Iniciar sesión</button>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
+            aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        </div>
+      </nav>
+      <RegistreForm />
+      <LogInForm />
+    </>
   )
 }
 
