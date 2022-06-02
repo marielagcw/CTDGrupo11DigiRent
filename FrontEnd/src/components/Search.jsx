b1da14aac6dba29e6d373956a14a793c015fec6a
AguRivarola <agurivarola@gmail.com>
2022-06-01 21:34:55 -0300

    Agrego ciudades de un json en el buscador y simplifico algo de codigo en el componente 'Search' aparte de agregar State de formulario

--- FrontEnd/src/components/Search.jsx
+++ FrontEnd/src/components/Search.jsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot, faCalendar } from "@fortawesome/free-solid-svg-icons";
+import ciudades from "../productos.json";
 import '../styles/Navbar.css'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
@@ -11,20 +12,33 @@ import '../styles/Search.css'
 const Search = () => {
 
     const [widthWindow, setWidthWindow] = useState(0);
+    const [formData, setFormData] = useState({})
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        alert("Procesando Solicitud!\n" +
+            "Buscando en: " + formData.ciudad +
+            "\nCon fecha: " + formateDate(fecha))
+    }
+    const handleChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    }
     useEffect(() => {
-      const detectarWidth = (e) => { setWidthWindow(window.visualViewport.width)};
-      window.addEventListener('resize', (e) => detectarWidth())
-      return () => {
-        window.removeEventListener('resize', detectarWidth())
-      }
+        const detectarWidth = (e) => { setWidthWindow(window.visualViewport.width) };
+        window.addEventListener('resize', (e) => detectarWidth())
+        return () => {
+            window.removeEventListener('resize', detectarWidth())
+        }
     }, [widthWindow])
 
-    const [value, onChange] = useState(new Date([]));
-    
-    const sendCalendar = (e) =>{
+    const [fecha, setfecha] = useState(new Date([]));
+
+    const sendCalendar = (e) => {
         e.preventDefault()
-        if(value.length > 1){
+        if (fecha.length > 1) {
             let container = document.querySelector('.calandary-container')
             container.classList.add('d-none')
             container.classList.remove('form-flex')
@@ -37,23 +51,42 @@ const Search = () => {
         container.classList.toggle('form-flex')
     }
 
+    const formateDate = (dateSinFormato) => {
+        let fInicio, fFin;
 
-  return (<>
-    <div className="searchContainer">
-            <form action="POST" className='d-flex align-items-center pt-3'>
+        [fInicio, fFin] = dateSinFormato;
+
+        let dateFormateada = fInicio.getDate() + "/" + (fInicio.getMonth() + 1) +
+            " - " + fFin.getDate() + "/" + (fFin.getMonth() + 1);
+
+
+        return dateFormateada;
+    }
+    //Array ciudades
+    let ciudadesList = ciudades.ciudades;
+
+
+    return (<>
+        <div className="searchContainer">
+            <form action="POST" onSubmit={handleSubmit} className='d-flex align-items-center pt-3'>
                 <div className="iconInput">
-                    <input className='input-search' type="text"  placeholder='¿A donde vamos?' />
+                    <input className='input-search' type="text" list="ciudades" placeholder='¿A donde vamos?' name='ciudad' onChange={handleChange} />
                     <span className='icon iconLocation'>
-                        <FontAwesomeIcon icon={faLocationDot}/>
+                        <FontAwesomeIcon icon={faLocationDot} />
                     </span>
+                    <datalist id="ciudades">
+                        {ciudadesList.map((e, i) =>
+                            <option fecha={e} key={"ciudad_" + i} >{e}</option>
+                        )}
+                    </datalist>
                 </div>
                 <div className="iconInput">
-                    <input type="text" className='ms-2 input-search' name="date" id="input-calendar" placeholder='Check in - Check out' value={value[0] ? `${value[0].getDate()}/${value[0].getMonth() + 1} - ${value[1].getDate()}/${value[1].getMonth() + 1}`: ''}/>
+                    <input type="text" className='ms-2 input-search' name="date" id="input-calendar" placeholder='Check in - Check out' value={(fecha[0] ? formateDate(fecha) : '')} onChange={handleChange} />
                     <span onClick={showCalendar} className='icon iconCalender'>
-                        <FontAwesomeIcon icon={faCalendar}/>
+                        <FontAwesomeIcon icon={faCalendar} />
                     </span>
                     <div className='calandary-container d-none form-absolute'>
-                        <Calendar showDoubleView={widthWindow > 414 ? true : false} selectRange={true} onChange={onChange} defaultValue={Date([]) | Date()} />
+                        <Calendar showDoubleView={widthWindow > 414} selectRange={true} onChange={setfecha} />
                         <button onClick={sendCalendar} className='btn btn-primary btn-lg btn-calendary'>Aplicar</button>
                     </div>
                 </div>
@@ -63,11 +96,11 @@ const Search = () => {
         </div>
 
 
-    
-  </>
+
+    </>
 
 
-  )
+    )
 }
 
 export default Search
