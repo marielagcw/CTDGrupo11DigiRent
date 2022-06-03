628df5c62457ffd571eef41d2fc757c48546f9f9
AguRivarola <agurivarola@gmail.com>
2022-06-03 20:07:12 -0300

    Unifico versiones con desarrollo

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,54 +1,55 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
-import { slide as Menu } from 'react-burger-menu'
+import RegistreForm from './RegistreForm';
 import logo from '../img/Logo.png'
 import '../styles/Header.css'
-import { Link, useNavigate } from 'react-router-dom';
+import { displayForm } from '../scripts/displayForm';
+import LogInForm from './LogInForm';
 
 
-const Header = ({ btn }) => {
-  const navigate = useNavigate();
-
+const Header = () => {
   //controla tamaño ventana
   const [widthWindow, setWidthWindow] = useState(0);
 
   useEffect(() => {
-    const detectarWidth = () => { setWidthWindow(window.visualViewport.width) };
+    const detectarWidth = () => { setWidthWindow(window.visualViewport.width)};
     window.addEventListener('resize', () => detectarWidth())
     return () => {
       window.removeEventListener('resize', detectarWidth())
     }
   }, [widthWindow])
 
+  const handleCreateAccountLogIn = (e) => {
+    displayForm(e)
+  }
+
   const toggleVisibility = () => {
     document.querySelector('.session-manager').classList.toggle('display-none');
+  }
+  const returnHome = () =>{
+    window.location.reload();
   }
 
   return (
     <>
       <nav className='navbar navbar-light'>
         <div className='container-fluid'>
-          <div className='logo d-flex align-items-center' onClick={() => navigate('/')}>
+          <div className='logo d-flex align-items-center' onClick={returnHome}>
             <div className='image-container d-flex align-items-center'>
               <img src={logo} className='img-fluid' alt='logo' />
             </div>
             <p className='ps-2 fw-bold mb-0'>Digi <span className='bg-tertiary fw-bold text-light ps-1 pe-1'>Rent</span></p>
           </div>
-          <div className={widthWindow > 452 ? 'session-manager' : 'session-manager display-none'}>
-            {btn !== "login" && <Link to='/register'>
-              <button className='btn btn-lg btn-border-primary' id='createAcount' >Crear cuenta</button>
-            </Link>}
-            {btn !== "register" && <Link to='/login'>
-              <button className='btn btn-lg btn-border-primary ms-2 me-2' id='logIn' >Iniciar sesión</button>
-            </Link>}
+          <div className={widthWindow>452?'session-manager':'session-manager display-none'}>
+            <button className='btn btn-lg btn-border-primary' id='createAcount' onClick={handleCreateAccountLogIn}>Crear cuenta</button>
+            <button className='btn btn-lg btn-border-primary ms-2 me-2' id='logIn' onClick={handleCreateAccountLogIn}>Iniciar sesión</button>
           </div>
-          {widthWindow <= 452 && <Menu right width={'200px'} styles={{height:'20%'}}>
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/login">Log In</a>
-            <a id="contact" className="menu-item" href="/register">Register</a>
-          </Menu>}
+          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent15'
+            aria-controls='navbarSupportedContent15' aria-expanded='false' aria-label='Toggle navigation' onClick={toggleVisibility}><span className='navbar-toggler-icon' ></span></button>
         </div>
       </nav>
+      <RegistreForm />
+      <LogInForm />
     </>
   )
 }
