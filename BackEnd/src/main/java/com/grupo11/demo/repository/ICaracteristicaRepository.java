5b5d2c7ea520460c701e80ce905f5e52506459b6
Mariela Goncalves Colaco <marielagcw@hotmail.com>
2022-06-14 18:48:15 -0300

    Rama nueva feature back

--- BackEnd/src/main/java/com/grupo11/demo/repository/ICaracteristicaRepository.java
+++ BackEnd/src/main/java/com/grupo11/demo/repository/ICaracteristicaRepository.java
@@ -2,6 +2,7 @@ package com.grupo11.demo.repository;
 
 import com.grupo11.demo.model.Caracteristica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
