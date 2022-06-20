b9cc9ce088fd1f806afc6f5385e1840cfc9a4825
AguRivarola <agurivarola@gmail.com>
2022-06-20 02:44:03 -0300

    Busqueda por ciudad funcional

--- FrontEnd/src/components/ProductoList.jsx
+++ FrontEnd/src/components/ProductoList.jsx
@@ -9,7 +9,7 @@ const ProductoList = ({ search }) => {
 
     let url = "http://localhost:8080/productos/listarTodosRandom?size=8&page=0";
     if (search) {
-        url = "http://localhost:8080/productos/productosCiudad/"+search;
+        url = "http://localhost:8080/productos/productosCiudad/listarTodos";
     }
 
     let { data, isPending, error } = useFetch(url);
@@ -21,10 +21,18 @@ const ProductoList = ({ search }) => {
             <div className="category-container m-3">
                 <h2 className='list-title category'>Recomendaciones</h2>
                 <div className='d-flex justify-content-between align-item-center flex-wrap'>
-                    {!data ? <Spinner /> : data.map((prod, i) => {
-                        return (i < 8 &&
-                            <ProductoCard info={prod} key={"prod" + i} />)
-                    })}
+                    {!data ?
+                        <Spinner /> :
+                        !search ?
+                            data.map((prod, i) => {
+                                return (i < 8 &&
+                                    <ProductoCard info={prod} key={"prod" + i} />)
+                            }) :
+                            data.map((prod, i) => {
+                                return (
+                                    prod.ciudad.provincia.includes(search)&&
+                                    <ProductoCard info={prod} key={"prod" + i} />)
+                            })}
                 </div>
             </div>
         </>
