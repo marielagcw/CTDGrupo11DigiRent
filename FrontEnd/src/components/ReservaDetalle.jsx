bf86f44f1f1109b647decd56c7009735a1ad19d5
Natali Correa <natalicorrea93@gmail.com>
2022-06-23 20:51:50 -0300

    agrego ultimos detalles de reserva

--- FrontEnd/src/components/ReservaDetalle.jsx
+++ FrontEnd/src/components/ReservaDetalle.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 // import {Navigate, useNavigate} from 'react-router-dom';
 import '../styles/ReservaDetalle.css';
-import detalle from '../productos.json';
+// import detalle from '../productos.json';
 
 
 const ReservaDetalle = () =>{
-    let detalleReserva = detalle.detalle;
+    // let detalleReserva = detalle.detalle;
     // const navigate = useNavigate();
 
     return (
@@ -15,17 +15,25 @@ const ReservaDetalle = () =>{
                 <div id="imgdetalle">
                 <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG9zdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500" className="imagendetalle" alt="" />
                 </div>
-                <div>
+                <div className="informacionReserva m-3">
+                    <h6 className="categoriaReserva">Cabañas</h6>
+                    <h3 className='titleReserva'>"Cabaña en montaña"</h3>
+                    
+                    <h6 className='ubicacionReserva'>UBICACION</h6>
+                    <hr></hr>
+                    <h5 className='entradaReserva'>Chek-in</h5>
+                    <hr></hr>
+                    <h5 className='entradaReserva'>Chek-Out</h5>
+                    <hr></hr>
+                    <div className="containerReservaConf">
+                        <button className='btn btn-primary btn-lg btn-max-width'>Confirmar reserva</button>
+                    </div>
+                    
+                    {/* <button type="button" className="btn btn-primary" onClick={()=>navigate('/products')}>Confirmar Reserva</button> */}
+
+                    
 
                 </div>
-                
-                {/* <div className="card-body ">
-                    <h3 className='title'>{info.title}</h3>
-                    <h4 className='category'>{info.category}</h4>
-                    <h5 className='location'>{info.location}</h5>
-                    <p className="card-text description">{info.description}</p>
-                    <button type="button" className="btn btn-primary" onClick={()=>navigate('/products')}>Confirmar Reserva</button>
-                </div> */}
                 
             </div>
         </>
