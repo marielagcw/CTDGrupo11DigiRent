a232c20d428c024a02eef0b418969265be78767d
JonatanMerlo <jonymerlo14@gmail.com>
2022-07-04 17:13:05 -0300

    Acomodo visualmente la lista de ciudades

--- FrontEnd/src/components/Search.jsx
+++ FrontEnd/src/components/Search.jsx
@@ -202,6 +202,7 @@ const Search = ({ busqueda }) => {
     }
 
 
+
     //Array ciudades
     let ciudadesList = [];
     let url = "http://localhost:8080/ciudades/listarTodos?ord=ASC&field=id";
@@ -222,13 +223,22 @@ const Search = ({ busqueda }) => {
                     <span className='icon iconLocation'>
                         <FontAwesomeIcon icon={faLocationDot} />
                     </span>
-                    <input onChange={handleChange} autoComplete='off' name="ciudad" className='input-search' placeholder='Ingrese una ciudad' id="ciudades" />
+                    <input onChange={handleChange} onBlur={displayNone} autoComplete='off' name="ciudad" className='input-search' placeholder='Ingrese una ciudad' id="ciudades" />
                         <div hidden={true} className='displayOptions'>
                             {ciudadesFiltradas !== [''] ? 
                                 ciudadesFiltradas.map((e, i) =>
                                     (
                                         <>
-                                            <div className="iconInput ciudad" fecha={e} onClick={displayNone} key={i} >{e}</div>
+                                        <div className='cityContainer'>
+                                            <span className='cityIcon'>
+                                                <FontAwesomeIcon icon={faLocationDot} />
+                                            </span>
+                                            <div className="ciudad" fecha={e} onClick={displayNone} key={i} >
+                                                <p className='cityName' style={{margin:"0px"}}>{e}</p> <p className='countryName' style={{margin:'0px'}}>Argentina</p>
+                                            </div> 
+
+                                        </div>
+                                            
                                         </>
                                     )
                                     
@@ -236,7 +246,14 @@ const Search = ({ busqueda }) => {
                                 (ciudadesList.map((e, i) =>
                                     (
                                         <>
-                                            <div className="iconInput ciudad" fecha={e} onClick={displayNone} key={"ciudad_" + i} >{e}</div>
+                                        <div className='cityContainer'>
+                                            <span className='cityIcon'>
+                                                <FontAwesomeIcon icon={faLocationDot} />
+                                            </span>
+                                            <div className="ciudad" fecha={e} onClick={displayNone} key={i} >
+                                                <p className='cityName' style={{margin:"0px"}}>{e}</p> <p className='countryName' style={{margin:'0px'}}>Argentina</p>
+                                            </div> 
+                                        </div>
                                         </>
                                     )
                                     
