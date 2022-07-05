37bdc83e65843b4fc31c4c3c921a729ddcd522e1
Natali Correa <natalicorrea93@gmail.com>
2022-07-04 23:36:11 -0300

    en proceso

--- FrontEnd/src/components/TemplateReservaExitosa.jsx
+++ FrontEnd/src/components/TemplateReservaExitosa.jsx
@@ -13,9 +13,10 @@ export default function Reserva() {
           <div className="templatereserva text-center">
           <div className="templ-body">
             <div id="textocentrado">
-            <h5 className="templ-title">¡Muchas gracias!</h5>
-            <p  className="templ-text">
-              Su reserva se ha realizado con éxito.
+              <p icon="fa-solid fa-badge-check" >ICONO </p>
+              <h5 className="templ-title">¡Muchas gracias!</h5>
+              <p  className="templ-text">
+                Su reserva se ha realizado con éxito.
             </p>
             
             <a href="#" className="btn btn-primary btn-lg" onClick={()=>navigate('/')}>
