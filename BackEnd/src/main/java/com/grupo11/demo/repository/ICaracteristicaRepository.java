eb73463c4a52858a37ead115902333c05a62fd84
Mariela Goncalves Colaco <marielagcw@hotmail.com>
2022-07-10 16:56:21 -0300

    Creación pipeline para el deploy del front

--- BackEnd/src/main/java/com/grupo11/demo/repository/ICaracteristicaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo11.demo.repository;
-
-import com.grupo11.demo.model.Caracteristica;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICaracteristicaRepository  extends JpaRepository<Caracteristica, Integer> {
-}
--- BackEnd/src/main/java/com/grupo11/demo/repository/IElementoPoliticaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupo11.demo.repository;
-
-import com.grupo11.demo.model.ElementoPolitica;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IElementoPoliticaRepository extends JpaRepository<ElementoPolitica,Integer> {
-}
