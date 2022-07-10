24c999e78b99fe091a8b1a557a88e41fac7f485b
Mariela Goncalves Colaco <marielagcw@hotmail.com>
2022-07-10 16:47:37 -0300

    ReadMe

--- FrontEnd/src/components/MostrarSegunRol.jsx
+++
@@ -1,6 +0,0 @@
-import { getRol } from "../scripts/authService"
-
-export const MostrarSegunRol = ({rol, children})=>{
-    const autenticado = getRol() === rol
-    return autenticado?children:null
-}
